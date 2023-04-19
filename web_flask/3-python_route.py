47cb82222046fded05a1cc028207d14a1bc9804f
Emmanuel Chukwu <officialecho95@outlook.com>
2023-04-19 04:39:12 +0000

    python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,39 +1,47 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+"""
 from flask import Flask
-"""
-this module receives a text and prints it
-"""
 
-# Create a Flask app
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """function that contains the flask command"""
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """function that returns hbnb"""
-    return 'HBNB'
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def text(text):
-    """function that returns the value if of text"""
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
 
 
-@app.route('/python/', defaults={'text': 'is cool'})
-@app.route('/python/<string:text>', strict_slashes=False)
-def python_text(text):
-    """function that returns hbnb"""
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-if __name__ == '__main__':
-    # Run the app on 0.0.0.0, port 5000
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
