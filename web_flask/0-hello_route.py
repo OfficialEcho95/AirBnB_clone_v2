cf56fc67a4beafde1f12719b7e6291fd25b91cdf
Emmanuel Chukwu <officialecho95@outlook.com>
2023-04-19 03:03:37 +0000

    hello-route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,10 +8,13 @@ from flask import Flask
 app = Flask(__name__)
 
 # Route for the root URL
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     """function that contains the flask command"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     # Run the app on 0.0.0.0, port 5000
