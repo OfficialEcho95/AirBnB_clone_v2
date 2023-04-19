ed81c75908690a700a7b52d381e96172510dc54c
Emmanuel Chukwu <officialecho95@outlook.com>
2023-04-19 03:29:54 +0000

    C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,6 +19,12 @@ def hello_hbnb():
     """function that returns hbnb"""
     return 'HBNB'
 
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """function that returns hbnb"""
+    return 'C ' % escape(text)
+
 if __name__ == '__main__':
     # Run the app on 0.0.0.0, port 5000
     app.run(host='0.0.0.0', port=5000)
